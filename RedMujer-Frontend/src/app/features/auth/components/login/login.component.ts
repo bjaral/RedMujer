b627a91e9e7dcba275d36cadbc2b79f6497a7d8c
Paula <psantanav0@gmail.com>
2025-05-13 23:30:00 -0400

    Seccion Sign In en el Login

--- RedMujer-Frontend/src/app/features/auth/components/login/login.component.ts
+++ RedMujer-Frontend/src/app/features/auth/components/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  hidePassword = true;
 }
