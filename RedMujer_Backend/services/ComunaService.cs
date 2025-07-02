c11e74cf71cdfc7d157b756443d016d9ade2a29d
GudmarSQ <gudmarpa13@gmail.com>
2025-07-02 03:12:33 -0400

    Mejoras en emprendimiento y comunas

--- RedMujer_Backend/services/ComunaService.cs
+++ RedMujer_Backend/services/ComunaService.cs
@@ -2,6 +2,7 @@ using RedMujer_Backend.DTOs;
 using RedMujer_Backend.models;
 using RedMujer_Backend.repositories;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace RedMujer_Backend.services
@@ -9,16 +10,62 @@ namespace RedMujer_Backend.services
     public class ComunaService : IComunaService
     {
         private readonly IComunaRepository _repo;
+
         public ComunaService(IComunaRepository repo)
         {
             _repo = repo;
         }
 
-        public async Task<IEnumerable<Comuna>> GetAllAsync() => await _repo.GetAllAsync();
-        public async Task<Comuna?> GetByIdsAsync(int id_region, int id_comuna)
+        public async Task<IEnumerable<ComunaDto>> GetAllAsync()
         {
-            return await _repo.GetByIdsAsync(id_region, id_comuna);
+            var comunas = await _repo.GetAllAsync();
+            return comunas.Select(c => new ComunaDto
+            {
+                Id_Comuna = c.Id_Comuna,
+                Id_Region = c.Id_Region,
+                Nombre = c.Nombre,
+                Vigencia = c.Vigencia
+            });
         }
+
+        public async Task<ComunaDto?> GetByIdsAsync(int idRegion, int idComuna)
+        {
+            var comuna = await _repo.GetByIdsAsync(idRegion, idComuna);
+            if (comuna == null) return null;
+            return new ComunaDto
+            {
+                Id_Comuna = comuna.Id_Comuna,
+                Id_Region = comuna.Id_Region,
+                Nombre = comuna.Nombre,
+                Vigencia = comuna.Vigencia
+            };
+        }
+
+        public async Task<ComunaDto?> GetByIdAsync(int idComuna)
+        {
+            var comuna = await _repo.GetByIdAsync(idComuna);
+            if (comuna == null) return null;
+            return new ComunaDto
+            {
+                Id_Comuna = comuna.Id_Comuna,
+                Id_Region = comuna.Id_Region,
+                Nombre = comuna.Nombre,
+                Vigencia = comuna.Vigencia
+            };
+        }
+
+        public async Task<IEnumerable<ComunaDto>> ObtenerComunasPorRegionAsync(int idRegion)
+        {
+            var comunas = await _repo.ObtenerComunasPorRegionAsync(idRegion);
+            return comunas.Select(c => new ComunaDto
+            {
+                Id_Comuna = c.Id_Comuna,
+                Id_Region = c.Id_Region,
+                Nombre = c.Nombre,
+                Vigencia = c.Vigencia
+            });
+        }
+
         public async Task CrearAsync(ComunaDto dto)
         {
             var comuna = new Comuna
@@ -27,24 +74,24 @@ namespace RedMujer_Backend.services
                 Nombre = dto.Nombre,
                 Vigencia = dto.Vigencia
             };
-            await _repo.InsertAsync(comuna);
+            await _repo.CrearAsync(comuna);
         }
 
-        public async Task ActualizarAsync(int id, ComunaDto dto)
+        public async Task ActualizarAsync(int idComuna, ComunaDto dto)
         {
             var comuna = new Comuna
             {
-                Id_Comuna = id,
+                Id_Comuna = idComuna,
                 Id_Region = dto.Id_Region,
                 Nombre = dto.Nombre,
                 Vigencia = dto.Vigencia
             };
-            await _repo.UpdateAsync(comuna);
+            await _repo.ActualizarAsync(comuna);
         }
 
-        public async Task EliminarAsync(int id)
+        public async Task EliminarAsync(int idComuna)
         {
-            await _repo.DeleteAsync(id);
+            await _repo.EliminarAsync(idComuna);
         }
     }
 }
