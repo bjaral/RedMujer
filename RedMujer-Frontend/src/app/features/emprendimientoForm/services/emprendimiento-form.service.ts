6fee0635c75936e6d8e6f4384185590129bb487b
Paula <psantanav0@gmail.com>
2025-07-04 02:58:24 -0400

    Vista nuevo emprendimiento funcional

--- RedMujer-Frontend/src/app/features/emprendimientoForm/services/emprendimiento-form.service.ts
+++ RedMujer-Frontend/src/app/features/emprendimientoForm/services/emprendimiento-form.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class EmprendimientoFormService {
 
   constructor(private http: HttpClient) {}
 
-  crearEmprendimiento(data: any, imagen: File): Observable<any> {
+  crearEmprendimiento(data: any, imagen: File | null): Observable<any> {
     const formData = new FormData();
     formData.append('RUT', data.rut);
     formData.append('Nombre', data.nombre);
@@ -19,13 +19,15 @@ export class EmprendimientoFormService {
     formData.append('Modalidad', data.modalidad);
     formData.append('Horario_Atencion', data.horario_Atencion);
     formData.append('Vigencia', 'true');
-    formData.append('Imagen', imagen);
+    if (imagen) {
+      formData.append('Imagen', imagen);
+    }
 
     return this.http.post<any>(this.apiUrl, formData);
   }
 
   subirMultimedia(idEmprendimiento: number, archivos: File[], descripcion = '', tipo = 'imagen'): Observable<any[]> {
-    const uploadRequests = archivos.map((archivo) => {
+    const requests = archivos.map((archivo) => {
       const formData = new FormData();
       formData.append('Archivo', archivo);
       formData.append('Tipo_Multimedia', tipo);
@@ -34,23 +36,6 @@ export class EmprendimientoFormService {
       return this.http.post(`${this.apiUrl}/${idEmprendimiento}/multimedia`, formData);
     });
 
-    return new Observable(observer => {
-      const results: any[] = [];
-      let completed = 0;
-
-      uploadRequests.forEach((req, index) => {
-        req.subscribe({
-          next: res => {
-            results[index] = res;
-            completed++;
-            if (completed === uploadRequests.length) {
-              observer.next(results);
-              observer.complete();
-            }
-          },
-          error: err => observer.error(err)
-        });
-      });
-    });
+    return forkJoin(requests);
   }
 }
