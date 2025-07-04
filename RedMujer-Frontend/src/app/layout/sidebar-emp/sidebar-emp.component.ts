32edbde7dff3e891c39dbf5195d9c1a271843a75
Paula <psantanav0@gmail.com>
2025-07-04 04:41:33 -0400

    Sidebar emprendedoras funcional

--- RedMujer-Frontend/src/app/layout/sidebar-emp/sidebar-emp.component.ts
+++ RedMujer-Frontend/src/app/layout/sidebar-emp/sidebar-emp.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MATERIAL_IMPORTS } from '../../shared/material/material';
 
 @Component({
   selector: 'app-sidebar-emp',
-  imports: [RouterModule],
+  imports: [RouterModule, MATERIAL_IMPORTS],
   templateUrl: './sidebar-emp.component.html',
   styleUrl: './sidebar-emp.component.scss'
 })
+
 export class SidebarEmpComponent {
 
 }
