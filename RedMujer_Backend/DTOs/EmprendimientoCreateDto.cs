1f31607e20b10455e109903075bd440f50bfa8d5
GudmarSQ <gudmarpa13@gmail.com>
2025-07-05 22:28:34 -0400

    Mejoras en Emprendimiento

---
+++ RedMujer_Backend/DTOs/EmprendimientoCreateDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+namespace RedMujer_Backend.DTOs;
+public class EmprendimientoCreateDto
+{
+    public string? RUT { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public string Modalidad { get; set; } = "";
+    public string? Horario_Atencion { get; set; }
+    public bool Vigencia { get; set; }
+    public IFormFile? Imagen { get; set; }  // archivo subido
+}
--- RedMujer_Backend/DTOs/EmprendimientoDto.cs
+++ RedMujer_Backend/DTOs/EmprendimientoDto.cs
@@ -5,24 +5,14 @@ namespace RedMujer_Backend.DTOs
 {
     public class EmprendimientoDto
     {
-        [Required(ErrorMessage = "El RUT es obligatorio")]
-        [StringLength(50, ErrorMessage = "El RUT no debe superar los 50 caracteres")]
+        public int Id_Emprendimiento { get; set; }
         public string? RUT { get; set; }
-
-        [Required(ErrorMessage = "El nombre es obligatorio")]
-        [StringLength(50, ErrorMessage = "El nombre no debe superar los 50 caracteres")]
         public string Nombre { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "La descripción es obligatoria")]
         public string Descripcion { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "La modalidad es obligatoria")]
-        public required string Modalidad { get; set; }
-
+        public string Modalidad { get; set; } = "";
         public string? Horario_Atencion { get; set; }
-
         public bool Vigencia { get; set; }
+        public string? Imagen { get; set; }  // aquí va la ruta/url de la imagen
 
-        public IFormFile? Imagen { get; set; }
     }
 }
