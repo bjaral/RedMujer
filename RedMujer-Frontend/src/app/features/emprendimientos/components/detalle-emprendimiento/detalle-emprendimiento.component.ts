126552d93a05005fe91af57ab861ec869047f91c
Don-Wea <podlech@live.com.ar>
2025-07-07 02:45:37 -0400

    WIP

--- RedMujer-Frontend/src/app/features/emprendimientos/components/detalle-emprendimiento/detalle-emprendimiento.component.ts
+++ RedMujer-Frontend/src/app/features/emprendimientos/components/detalle-emprendimiento/detalle-emprendimiento.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { MATERIAL_IMPORTS } from '../../../../shared/material/material';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { EmprendimientoService } from '../../services/emprendimiento.service';
 
 @Component({
   selector: 'app-detalle-emprendimiento',
-  imports: [],
+  imports: [MATERIAL_IMPORTS, CommonModule],
   templateUrl: './detalle-emprendimiento.component.html',
   styleUrl: './detalle-emprendimiento.component.scss'
 })
-export class DetalleEmprendimientoComponent {
 
+
+export class DetalleEmprendimientoComponent {
+  idEmprendimiento!: number;
+
+  nombre = "";
+  categoria = "";
+  Instagram = "";
+  Facebook = "";
+  X = "";
+  Whatsapp = "";
+
+  web = "";
+  ubicacion = "";
+
+  constructor(private emprendimeinto: EmprendimientoService ,private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idEmprendimiento = +params['id'];
+    });
+    this.getEmprendimiento();
+  }
+
+  getEmprendimiento() {
+    this.emprendimeinto.getById(this.idEmprendimiento).subscribe({
+      next: (data) => {
+        this.nombre = data.nombre;
+      }
+    })
+  }
 }
