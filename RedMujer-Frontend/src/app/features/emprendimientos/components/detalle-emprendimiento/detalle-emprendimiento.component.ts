2c17c1849590f48afa4e2889c1a1a51ae1f0905f
Don-Wea <podlech@live.com.ar>
2025-07-08 00:55:13 -0400

    Primera versión de detalle-emprendimiento

--- RedMujer-Frontend/src/app/features/emprendimientos/components/detalle-emprendimiento/detalle-emprendimiento.component.ts
+++ RedMujer-Frontend/src/app/features/emprendimientos/components/detalle-emprendimiento/detalle-emprendimiento.component.ts
@@ -3,10 +3,13 @@ import { MATERIAL_IMPORTS } from '../../../../shared/material/material';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { EmprendimientoService } from '../../services/emprendimiento.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-detalle-emprendimiento',
-  imports: [MATERIAL_IMPORTS, CommonModule],
+  imports: [MATERIAL_IMPORTS, CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './detalle-emprendimiento.component.html',
   styleUrl: './detalle-emprendimiento.component.scss'
 })
@@ -23,9 +26,12 @@ export class DetalleEmprendimientoComponent {
   Whatsapp = "";
 
   web = "";
+  urlImagen = "";
   ubicacion = "";
 
-  constructor(private emprendimeinto: EmprendimientoService ,private router: Router, private route: ActivatedRoute) {}
+  links = []
+
+  constructor(private emprendimiento: EmprendimientoService ,private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -35,10 +41,40 @@ export class DetalleEmprendimientoComponent {
   }
 
   getEmprendimiento() {
-    this.emprendimeinto.getById(this.idEmprendimiento).subscribe({
+    this.emprendimiento.getById(this.idEmprendimiento).subscribe({
       next: (data) => {
         this.nombre = data.nombre;
       }
     })
+    this.emprendimiento.getByIdImg(this.idEmprendimiento).subscribe({
+      next: (data) => {
+        this.urlImagen = data.imagenes[0];
+      }
+    })
+    this.emprendimiento.getByIdRedes(this.idEmprendimiento).subscribe({
+      next: (data) => {
+        this.links = data.valor;
+      }
+    })
+  }
+
+
+
+
+  @ViewChild('myVideo') videoRef!: ElementRef<HTMLVideoElement>;
+
+  playVideo() {
+    const video: HTMLVideoElement = this.videoRef.nativeElement;
+    if (video.paused) {
+      video.play();    // Reproduce el video
+    } else {
+      video.pause();   // Pausa el video si está reproduciendo
+    }
+  }
+
+  onVideoPlay() {
+    const video: HTMLVideoElement = this.videoRef.nativeElement;
+    const playButton = document.querySelector('.play-button');
+    playButton?.classList.add('hidden'); // Oculta el botón al comenzar a reproducir
   }
 }
